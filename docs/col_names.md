9efc55d29aaeaf794ef3794b112c0971dcd82a9a
Raleigh Littles <Raleigh Littles>
2024-10-19 00:40:27 -0700

    parsed out FFL expiration dates..
    need to generate new columns for dataframe

---
+++ docs/col_names.md
@@ -0,0 +1,2 @@
+"FFL Type" in ffl_types.csv -> "LIC_TYPE"
+"Country Region Name" in country_region.csv -> "LIC_REGN"
--- src/ffl_analyzer.py
+++ src/ffl_analyzer.py
@@ -1,8 +1,25 @@
 import pandas # can't read tab-delimited CSV files with built-in `csv` module
 import pdb
 
+
+
+def get_dataframe(csv_filename) -> pandas.DataFrame:
+    return pandas.read_csv(csv_filename, sep="\t")
+
+
+def generate_ffl_expiration_dates()
+
+
 if __name__ == "__main__":
 
     csv_path = "../original/0924-ffl-list-complete.txt"
-    df = pandas.read_csv(csv_path, sep="\t")
-    pdb.set_trace()
+
+    ffl_df = get_dataframe(csv_path)
+
+    import pdb; pdb.set_trace()
+
+    print(f"Loaded dataframe with columns: {ffl_df.columns}")
+
+
+
+    
